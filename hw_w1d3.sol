615841c7170d6ed22b0e0a0003674ca573d55991
LIMT-BIT <rayjames9601@gmail.com>
2025-12-17 13:42:08 +0700

    Update hw_w1d3.sol

--- hw_w1d3.sol
+++ hw_w1d3.sol
@@ -1,62 +1,71 @@
-// SPDX-License-Identifier: GPL-3.0
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract Bank {
+    address public owner;
 
-contract Bank is Ownable {
+    // 每个地址的存款金额
+    mapping(address => uint256) public balanceOf;
 
-    address[3] public topUsers;
+    // 存款前三名（不排序）
+    address[3] public top3;
 
-    mapping(address => uint) deposited;
-
-    constructor() Ownable(msg.sender) {}
-
-    function renewTopUsers(address _user) internal {
-        for (uint i = 0; i < 3; i++){
-            if (topUsers[i] == _user){
-                return ;
-            }
-        }
-
-        for (uint i = 0; i < 3; i++){
-            if (topUsers[i] == address(0)){
-                topUsers[i] = _user;
-                return ;
-            }
-        }
-
-        uint lowestIndex = findLowestIndex();
-
-        if (deposited[_user] > deposited[topUsers[lowestIndex]]){
-            topUsers[lowestIndex] = _user;
-
-        }
-
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not owner");
+        _;
     }
 
-    function findLowestIndex() internal view returns(uint) {
-        uint lowestIndex = 0;
-        for (uint i = 1; i < 3; i++){
-            if(deposited[topUsers[i]] < deposited[topUsers[lowestIndex]]){
-                lowestIndex = i;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    /* ========== 存款逻辑 ========== */
+
+    receive() external payable {
+        require(msg.value > 0, "zero deposit");
+
+        balanceOf[msg.sender] += msg.value;
+        _updateTop3(msg.sender);
+    }
+
+    /* ========== 更新前三名 ========== */
+
+    function _updateTop3(address user) internal {
+        // 1. 如果已经在 top3，直接返回
+        for (uint i = 0; i < 3; i++) {
+            if (top3[i] == user) {
+                return;
             }
         }
-        return lowestIndex;
+
+        // 2. 找到当前 top3 中余额最小的索引
+        uint minIndex = 0;
+        uint minBalance = balanceOf[top3[0]];
+
+        for (uint i = 1; i < 3; i++) {
+            if (balanceOf[top3[i]] < minBalance) {
+                minBalance = balanceOf[top3[i]];
+                minIndex = i;
+            }
+        }
+
+        // 3. 如果新用户余额更大，替换最小的
+        if (balanceOf[user] > minBalance) {
+            top3[minIndex] = user;
+        }
     }
+
 
     function withdraw() external onlyOwner {
-        // payable(owner()).transfer(address(this).balance);
-        (bool sent, ) = payable(owner()).call{value: address(this).balance}("");
-        require(sent);
+        uint amount = address(this).balance;
+        require(amount > 0, "no eth");
+
+        (bool ok, ) = payable(owner).call{value: amount}("");
+        require(ok, "withdraw failed");
     }
 
 
-    receive() external payable { 
-        require(msg.value > 0);
-        deposited[msg.sender] += msg.value;
-        renewTopUsers(msg.sender);
-
-
+    function getTop3() external view returns (address[3] memory) {
+        return top3;
     }
 }
